414fb13ab26ccad422e16249a0a992c56408b396
alicia.brandewie@gmail.com <alicia@Alicia-PC.localdomain>
2025-06-23 16:56:37 -0500

    exercise-pseudo-code-ish

--- app.js
+++ app.js
@@ -1,4 +1,32 @@
 console.log("This is connected")
+
+/* User stories for this lab:
+
+As a user, I want to be able to select numbers so that I can perform operations with them.
+As a user, I want to be able to add two numbers together.
+As a user, I want to be able to subtract one number from another.
+As a user, I want to be able to multiply two numbers together.
+As a user, I want to be able to divide one number by another.
+As a user, I want to be able to see the output of the mathematical operation.
+As a user, I want to be able to clear all operations and start from 0.
+
+//Pseudo code
+
+Select two numbers; 
+    event listeners
+
+equation of two numbers (add, subtract, multiply, divide); 
+      event listeners
+
+output of equation;
+    functions
+
+Clear all operations & start from 0;
+    event listener + functions
+
+
+
+
 /*-------------------------------- Constants --------------------------------*/
 
 /*-------------------------------- Variables --------------------------------*/
